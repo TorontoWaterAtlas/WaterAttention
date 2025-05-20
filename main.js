2588d36d6332ae06481f65495d7559fbb09b237b
Taneea S Agrawaal <tannyaagrawal@gmail.com>
2025-05-20 14:29:39 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,63 @@
+import { Application, Assets, Sprite } from "pixi.js";
+import {BulgePinchFilter} from '@pixi/filter-bulge-pinch';
+import {Container} from 'pixi.js';
+
+(async () => {
+  // Create a new application
+  const app = new Application();
+
+  // Initialize the application
+  await app.init({
+    preference: 'webgl',background: "#1099bb", resizeTo: window });
+
+  // Append the application canvas to the document body
+  document.getElementById("pixi-container").appendChild(app.canvas);
+
+    // Load a texture (you can change this URL to any image)
+    const imageURL = 'https://raw.githubusercontent.com/TorontoWaterAtlas/WaterAttention/refs/heads/main/2025-05-05_13-49-19.png';
+
+    const texture = await Assets.load(imageURL);
+    const sprite = new Sprite(texture);
+    sprite.anchor.set(0.5);
+    sprite.x = app.screen.width / 2;
+    sprite.y = app.screen.height / 2;
+    // sprite.width = app.screen.width;
+    // sprite.height = app.screen.height;
+  // sprite.scale.set(1);
+    const container = new Container();
+    container.addChild(sprite);
+
+    // Apply BulgePinchFilter
+    const filter = new BulgePinchFilter({
+      center: [0.5, 0.5], // normalized center
+      radius: 900,
+      strength: 1,
+    });
+
+  sprite.filters = [filter];
+  app.stage.addChild(sprite);
+  app.renderer.on('error', console.error);
+
+    // Animate the bulge effect
+    let tick = 0;
+    app.ticker.add((time) => {
+      tick += time.deltaTime * 0.5
+      filter.strength = Math.sin(tick); // bulge to pinch
+      // console.log(filter.strength);
+      // (container.filters.map(filter => console.log(filter.strength)));
+    });
+  console.log(app.renderer.type);
+  app.stage.eventMode = 'static'; // enables interaction events
+  sprite.eventMode = 'static';
+
+  app.stage.on('pointermove', (event) => {
+    const global = event.global;
+    const x = global.x / app.screen.width;
+    const y = global.y / app.screen.height;
+    filter.center = [x, y];
+    // console.log(filter.center);
+    // console.log(filter.strength);
+  });
+
+
+})();
